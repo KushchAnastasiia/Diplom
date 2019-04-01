17acefa1f6a50e3f8a818b216b3a45032639fdda
KushchAnastasia <kavkiss13@gmail.com>
2019-04-02 02:19:15 +0300

    update view in accounts app urls

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'api/auth/register', RegisterAPI.as_view()),
     url(r'api/auth/login', LoginAPI.as_view()),
     url(r'api/auth/user', GetUserAPI.as_view()),
-    url(r'api/auth/logout', knox_views.LoginView.as_view(), name='knox-logout')
+    url(r'api/auth/logout', knox_views.LogoutView.as_view(), name='knox-logout')
 ]
