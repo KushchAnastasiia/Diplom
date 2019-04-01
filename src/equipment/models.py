9e0e06b38fc5c45b5189ec320936fbb7e9b3e72e
KushchAnastasia <kavkiss13@gmail.com>
2019-04-02 02:34:58 +0300

    update max digital to price and guarantee fields

--- src/equipment/models.py
+++ src/equipment/models.py
@@ -4,14 +4,20 @@ from django.db import models
 class Manufacturer(models.Model):
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Equipment(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
-    price = models.IntegerField(default=0)
+    price = models.DecimalField(default=0, decimal_places=3, max_digits=10)
     currency = models.CharField(max_length=16)
-    guarantee = models.IntegerField(default=0)
+    guarantee = models.DecimalField(default=0, decimal_places=3, max_digits=10)
     in_case = models.BooleanField(default=False)
+    manufacture = models.ForeignKey(
+        to=Manufacturer, on_delete=models.CASCADE
+    )
 
     type = models.CharField(max_length=64)
     group = models.CharField(max_length=64)
