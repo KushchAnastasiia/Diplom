ddc1ed866837dc65843370ab8643b788c9ffa720
KushchAnastasia <kavkiss13@gmail.com>
2019-04-02 15:21:04 +0300

    update get user data

--- src/accounts/api_views/GetUserAPI.py
+++ src/accounts/api_views/GetUserAPI.py
@@ -2,6 +2,8 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework import status
 from ..serializers import UserSerializer, LoginSerializer
+from personal_area.serializers import FeaturedEquipmentSerializer
+from personal_area.models import FeaturedEquipment
 
 
 class GetUserAPI(generics.RetrieveAPIView):
@@ -13,11 +15,12 @@ class GetUserAPI(generics.RetrieveAPIView):
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
-
+        featured_list = FeaturedEquipment.objects.filter(user_id=instance.id)
+        featured_serializer = FeaturedEquipmentSerializer(featured_list, many=True)
         return Response({
             'data': {
                 'user': serializer.data,
-                'featured': []
+                'featured': featured_serializer.data
             }
         }, status=status.HTTP_202_ACCEPTED)
 
