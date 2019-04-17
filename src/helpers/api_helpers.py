6558aea0bf0db2d72d0fb3d63601dd6256de65b7
KushchAnastasiia <kavkiss13@gmail.com>
2019-04-17 03:26:21 +0300

    add feature and accounts yaml paths

--- src/helpers/api_helpers.py
+++ src/helpers/api_helpers.py
@@ -15,11 +15,15 @@ def get_full_equipment_data(d=None):
         Country.objects.filter(id=int(manufacture["country"])).first()
     ).data
 
+    manufacture['country'] = country
+
     subgroup = SubGroupSerializer(
         SubGroup.objects.filter(id=int(d["subgroup"])).first()).data
 
     group = GroupSerializer(
         Group.objects.filter(id=int(subgroup["id"])).first()).data
+    
+    subgroup['group'] = group
 
     market_list = []
     market_id_list = EquipmentMarketConnection.objects.filter(equipment=d['id']).all()
@@ -31,8 +35,8 @@ def get_full_equipment_data(d=None):
     ctx['type_of'] = type_of
     ctx['manufacture'] = manufacture
     ctx['subgroup'] = subgroup
-    ctx['group'] = group
-    ctx['country'] = country
+    # ctx['group'] = group
+    # ctx['country'] = country
     ctx['markets'] = market_list
 
     return ctx
